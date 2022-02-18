5a4baeb7985d883cc3bf1562bbf7f1930c895cf0
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2022-02-18 22:59:34 +0700

    1.0

---
+++ login-reg/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hiku</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        <div class="p-5 mb-4 rounded-3 bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Selamat datang di WebsiteHiku</h1>
+                        <p>Bergabunglah bersama jutaan orang lainnya...</p>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="login.php" class="btn btn-secondary">Masuk</a>
+                        <a href="daftar.php" class="btn btn-success">Daftar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img class="img-fluid" src="img/conect.png" alt="gambar conect">
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
